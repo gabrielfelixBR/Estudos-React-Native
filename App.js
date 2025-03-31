477ed2e91d394981dbaa1c45c360f1b956740266
Felix <mrgabrielfelixsilva@gmail.com>
2025-03-31 13:49:12 -0300

    Slider

--- App.js
+++ App.js
@@ -1,46 +1,21 @@
-import React, { useRef } from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
-import { Picker } from "@react-native-picker/picker";
+
+import Slider from '@react-native-community/slider'
 
 export default function App() {
-  const [carroSelecionado, setCarroSelecionado] = useState(null);
-  const pickerRef = useRef();
-
-  const [carros, setCarros] = useState([
-    { key: "0", nome: "golf 1.6", valor: "62.000" },
-    { key: "1", nome: "savero 1.0", valor: "29.300" },
-    { key: "2", nome: "gol 1.0", valor: "25.200" },
-    { key: "3", nome: "bmw 120i", valor: "225.200" },
-    { key: "4", nome: "uno com escada em cima", valor: "30.000" },
-  ]);
+  const [valor, setValor] = useState(50)
 
   return (
     <View style={styles.container}>
-      <Picker
-        ref={pickerRef}
-        selectedValue={carroSelecionado}
-        onValueChange={(itemValue) => setCarroSelecionado(itemValue)}
-        style={styles.picker}
-      >
-        <Picker.Item label="Selecione um carro" value="0" />
-        {carros.map((v, k) => (
-          <Picker.Item label={v.nome} value={k} key={v.key} />
-        ))}
-      </Picker>
+      <Slider
+        minimumValue={0}
+        maximumValue={100}
+        value={valor}
+        onValueChange={(valorSeleconado) => setValor(valorSeleconado)}
+      />
 
-      <View style={styles.infoContainer}>
-        {carroSelecionado !== null && (
-          <>
-            <Text style={styles.carros}>
-              carro: {carros[carroSelecionado].nome}
-            </Text>
-            <Text style={styles.carros}>
-              R$ {Number(carros[carroSelecionado].valor).toFixed(2)}
-            </Text>
-          </>
-        )}
-      </View>
+      <Text>{valor}</Text>
     </View>
   );
 }
@@ -52,18 +27,5 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#fff",
     padding: 20,
-  },
-  picker: {
-    width: "100%",
-    borderRadius: 8,
-    marginBottom: 20,
-  },
-  infoContainer: {
-    alignItems: "center",
-  },
-  carros: {
-    fontSize: 22,
-    textAlign: "center",
-    marginVertical: 4,
   },
 });
